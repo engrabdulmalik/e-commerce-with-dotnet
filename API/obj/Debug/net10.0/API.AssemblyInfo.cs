53daeb19fad02e0e768ef0a36fa0f61b4652c06a
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-08-28 09:43:48 +0500

    seeding done

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27130a31a8f461e6b8b8cf23c640f0ad3b7bb508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e08552fabf963a6347bd7f383b20e9e296c8021")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
