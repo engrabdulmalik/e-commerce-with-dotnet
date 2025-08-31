08878e951b1f9312e6aa3822932870c0af3f70ad
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-08-31 07:29:45 +0500

    section 5 start

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8ead6623c7896be18226c9bb072c63fd9d1fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057a6881d3ed192a3b103951857ad37cd668b417")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
